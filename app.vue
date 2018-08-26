5b717fdd3bf5790677c5c80aa3ddeaa4340b1e53
kaiqiw <1132620342@qq.com>
2018-08-26 21:54:47 +0800

    node-sass sass-loader

---
+++ app.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <gButton></gButton>
+        <gButton></gButton>
+        <gButton></gButton>
+        <gButton></gButton>
+    </div>
+</template>
+
+<script>
+import gButton from './src/button'
+export default {
+  components: {
+    gButton
+  }
+}
+</script>
+
+<style>
+</style>
